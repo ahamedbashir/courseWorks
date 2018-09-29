7e96988a235c8f4c97c16273fb6db4d78a70b01f
Bashir Ahamed <ahamedb1991@gmail.com>
2018-09-29 18:03:48 -0400

    moved hw into Assignments directory

--- hw2/HW_sharpen.cpp
+++
@@ -1,9 +0,0 @@
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// HW_sharpen:
-//
-// Sharpen image I1. Output is in I2.
-//
-void
-HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
-{
-}
