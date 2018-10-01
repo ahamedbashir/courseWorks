f95a1fff32d1ec312eedbab45af381ed8006b75b
Bashir Ahamed <ahamedb1991@gmail.com>
2018-09-30 20:13:45 -0400

    histogram stretch initial solution, works with sligth different outut

--- Assignments/hw1/HW_histoStretch.cpp
+++ Assignments/hw1/HW_histoStretch.cpp
@@ -7,4 +7,30 @@
 void
 HW_histoStretch(ImagePtr I1, int t1, int t2, ImagePtr I2)
 {
+
+	// Image header
+	IP_copyImageHeader(I1, I2);
+
+	int w = I1->width();
+	int h = I1->height();
+	int total = w*h;
+
+	int i, lut[MXGRAY];
+
+	int minMaxDiff = t2 - t1;
+	for(i = 0; i < MXGRAY; ++i)
+		lut[i] = CLIP( round((double)(i-t1)/minMaxDiff * MaxGray) ,0,MaxGray);
+
+	
+	ChannelPtr<uchar> p1, p2, endPtr;
+	int type;
+
+	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+		IP_getChannel(I2, ch, p2, type);
+		for(endPtr = p1 + total; p1 < endPtr; )
+			*p2++ = lut[*p1++];
+	}
+
+
+
 }
