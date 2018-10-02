12da5e9b900ac455fbc3ea50198954f920f709c6
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-02 11:33:41 -0400

    Contrast enhancement redone and fixed

--- Assignments/hw1/HW_contrast.cpp
+++ Assignments/hw1/HW_contrast.cpp
@@ -9,20 +9,22 @@
 void
 HW_contrast(ImagePtr I1, double brightness, double contrast, ImagePtr I2)
 {
-  IP_copyImageHeader(I1, I2);
+	IP_copyImageHeader(I1, I2);
+	int w = I1->width();
+	int h = I1->height();
+	int total = w*h;
 
-  int w = I1->width();
-  int h = I1->height();
-  int total = w * h;
+	int i, lut[MXGRAY];
 
-  int i, lut[MXGRAY];
-  for(i=0; i<MXGRAY; i++) lut[i] = std::min(std::max((int)std::round((i - 128)*contrast + 128 + brightness),0),255);
+	for(i = 0; i <MXGRAY; ++i) 
+		lut[i] = CLIP( round((i-128)*contrast + 128 + brightness), 0, MaxGray);
 
-  ChannelPtr<uchar> p1, p2;
-  int type;
+	ChannelPtr<uchar> p1, p2, endPtr;
+	int type;
 
-  for(int ch=0; IP_getChannel(I1, ch, p1, type); ch++)
-    IP_getChannel(I2, ch, p2, type);
-
-  for(i=0; i<total; i++) *p2++ = lut[*p1++];
+	for( int ch = 0; IP_getChannel(I1, ch, p1, type); ++ch) {
+		IP_getChannel(I2, ch, p2, type);
+		for(endPtr = p1 + total; endPtr > p1; )
+			*p2++ = lut[*p1++];
+	}
 }
