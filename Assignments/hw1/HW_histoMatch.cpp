e48cb18bc1f61075d1fbb6364c5318697768a20d
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-03 12:20:00 -0400

    problem with targeHisto histogram

--- Assignments/hw1/HW_histoMatch.cpp
+++ Assignments/hw1/HW_histoMatch.cpp
@@ -9,7 +9,6 @@ void histoMatchApprox(ImagePtr, ImagePtr, ImagePtr);
 void
 HW_histoMatch(ImagePtr I1, ImagePtr targetHisto, bool approxAlg, ImagePtr I2)
 {
-	std::cout << approxAlg << '\n';
 	if(approxAlg) {
 		// this is required
 
@@ -118,15 +117,15 @@ HW_histoMatch(ImagePtr I1, ImagePtr targetHisto, bool approxAlg, ImagePtr I2)
 void
 histoMatchApprox(ImagePtr I1, ImagePtr targetHisto, ImagePtr I2)
 {
+	// apply transformatio
 	IP_copyImageHeader(I1, I2);
-
-	// computer histogram of I1
 	int w = I1->width();
 	int h = I1->height();
-	
 	int total = w * h;
+
+	// Compute Histogram of I1
 	int H[MXGRAY];
-	double avgH[MXGRAY];
+	double CDF[MXGRAY];
 
 	for(int i = 0; i < MXGRAY; ++i)
 		H[i] = 0;
@@ -140,76 +139,63 @@ histoMatchApprox(ImagePtr I1, ImagePtr targetHisto, ImagePtr I2)
 
 	// normalize histogram of I1
 	for(int i = 0; i < MXGRAY; ++i) 
-		avgH[i] = H[i] /(double)total; 
+		CDF[i] = H[i] /(double)total; 
 
 	
 	// compute  CDF of I1
-	double CDF[MXGRAY];
-	CDF[0] = avgH[0];
-
 	for(int i = 1; i < MXGRAY; ++i)
-		CDF[i] = CDF[i-1] + avgH[i];
+		CDF[i] = CDF[i-1] + CDF[i];
 
 	// compute Histogram of targetHisto
 
 	int refH[MXGRAY]; 
-	double avgRefH[MXGRAY];
+	double refCDF[MXGRAY];
 	int refTotal = targetHisto->width() * targetHisto->height();
 
         for(int i = 0; i < MXGRAY; ++i)
                 refH[i] = 0;
 
         for( int ch = 0 ; IP_getChannel(targetHisto, ch, p2, type); ch++) {
-                for(int i = 0; i < MXGRAY; ++i){
-                        refH[*p1++]++;
+                for(int i = 0; i < refTotal; ++i){
+                        refH[*p2++]++;
 		}
         }
 
 	// normalize targetHisto
         
 	for(int i = 0; i < MXGRAY; ++i){
-                avgRefH[i] = refH[i] /(double)refTotal; 
+                refCDF[i] = refH[i] /(double)refTotal; 
 	}
   
 	// compute CDF of targetHisto
-	double refCDF[MXGRAY];
-
-
-
-	refCDF[0] = avgRefH[0];
 	for(int i = 1; i < MXGRAY; ++i)
-		refCDF[i] = refCDF[i-1] + avgRefH[i];
+		refCDF[i] = refCDF[i-1] + refCDF[i];
 
 
 	// create look up table
 	//
 	// CDF debugging
 	for(int i = 0; i < MXGRAY; ++i) 
-		std::cout <<i << " :  Ref Cdf = \t"<<refCDF[i] << "\t I1 CDF = " << CDF[i] << '\n';
-	
+		std::cout << i << " : refH = " << refH[i] << "\t Ref Cdf = \t"<<refCDF[i] << "\t I1 H  = " << H[i] << "\t CDF ="<< CDF[i] << '\n';
+
 		
 	int lut[MXGRAY];
 
-	for (int i = 0; i < MXGRAY; ++i) {
-		int l = MaxGray;
+	for (int l1 = 0; l1 < MXGRAY; ++l1) {
+		int l2 = MaxGray;
 
 		do {
-			lut[i] = l;
-			l -= 1;
-		} while(l >= 0 && refCDF[l] >  CDF[i]);
+			lut[l1] = l2;
+			l2--;
+		} while(l1 >= 0 && refCDF[l2] >  CDF[l1]);
 	}
-	
-	/*debug lut 
-	std:: cout << "lut values :\n";
-	for(int i : lut)
-		std::cout << i << " ";
-*/
+
 	// update the output image pixel using lut
 		
 	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ++ch) {
 		IP_getChannel(I2, ch, p2, type);
 		for(endPtr = p1 + total; p1 < endPtr; )
 			*p2++ = lut[*p1++];
-	}		
+	}	
 
 }
