7f222e3173821e9fd7fd22caa89ec73cf2eef3bc
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-28 19:15:26 -0400

    fixed calling external function

--- Assignments/hw2/HW_sharpen.cpp
+++ Assignments/hw2/HW_sharpen.cpp
@@ -3,6 +3,14 @@
 //
 // Sharpen image I1. Output is in I2.
 //
+// how to call HW_blur ????
+
+// fixed it... using extern
+
+extern void row_Blur(ChannelPtr<uchar> p1, ChannelPtr<uchar> p2, int length, int width, int jump);
+void
+extern HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2);
+
 void
 HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
 {
@@ -17,10 +25,22 @@ HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
 
 	// no need to process if size is 1
 	if(size == 1) {
+		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+			IP_getChannel(I2, ch, p2, type);
+			for(endPtr = total + p1; p1 < endPtr; )
+				*p2++ = *p1++;
+		}
 	}
 
 	else {
 		// blur the image
+		// ? Why not call the HW_blur function... nice
 		// multiply with factor with the blurred diff and add with original value
+		HW_blur(I1, size, size, I2);
+		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+			IP_getChannel(I2, ch, p2, type);
+			for(endPtr = total + p1; p1 < endPtr; p1++)
+				*p2++ = CLIP( (factor * ( *p1 - *p2) + *p1), 0, MaxGray);
+		}
 	}
 }
