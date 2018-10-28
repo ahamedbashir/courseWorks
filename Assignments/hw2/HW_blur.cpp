bcfbbafdd328f6a38960a60179a9638214ba1a7d
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-27 20:52:49 -0400

    initial algo sketch

--- Assignments/hw2/HW_blur.cpp
+++ Assignments/hw2/HW_blur.cpp
@@ -9,4 +9,39 @@
 void
 HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 {
+	ImagePtr I1_temp;
+	IP_copyImageHeader(I1, I1_temp);
+	IP_copyImageHeader(I1, I2);
+
+	int w = I1->width();
+	int h = I1->height();
+	int total = w * h;
+
+	ChannelPtr<uchar> p1, p1_temp, p2, endPtr;
+
+	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++ ) {
+		IP_getChannel(I1_temp, ch, p1_temp, type);
+		IP_getChannel(I2, ch, p2, type);
+
+		if(filterW == 1) {
+			for(endPtr = p1 + total; p1 < endPtr; )
+				*p1_temp++ = *p1++;
+		}
+
+		if( filterW > 1) {
+			// blur rows y = 0 to y = h
+			// should I create a function for this
+		}
+
+		// blurr the temp image and output to I2
+		if( filterH == 1) {
+			for(endPtr = p1_temp + total; p1_temp < endPtr; )
+				*p2++ = *p1_temp++;
+		}
+
+		if(filterH > 1) {
+			// copy all the blurred rows
+			// should I create a function for this
+		}
+	}
 }
