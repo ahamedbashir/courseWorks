04fa65fa9a351e29566d905dac6c1d76ec3dfb15
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-28 17:41:18 -0400

    sharpen initial commit

--- Assignments/hw2/HW_sharpen.cpp
+++ Assignments/hw2/HW_sharpen.cpp
@@ -6,4 +6,21 @@
 void
 HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
 {
+	int w = I1->width();
+	int h = I1->height();
+	int total = w * h;
+
+
+	IP_copyImageHeader(I1, I2);
+	ChannelPtr<uchar> p1, p2, endPtr;
+	int type;
+
+	// no need to process if size is 1
+	if(size == 1) {
+	}
+
+	else {
+		// blur the image
+		// multiply with factor with the blurred diff and add with original value
+	}
 }
