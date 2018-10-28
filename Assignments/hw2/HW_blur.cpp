aec813d351b5b698c255a83d67d2e2cb3df782e0
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-27 23:57:19 -0400

    blur works with slight different output

--- Assignments/hw2/HW_blur.cpp
+++ Assignments/hw2/HW_blur.cpp
@@ -6,6 +6,42 @@
 // We force the kernel dimensions to be odd.
 // Output is in I2.
 //
+
+void row_Blur(ChannelPtr<uchar> p1, ChannelPtr<uchar> p2, int length, int width, int jump) {
+	if(width % 2 == 0)
+		width++;
+
+	int numOfNeighbors = width / 2;
+	int bufSize = length + width - 1;
+
+	short* buf = new short[bufSize];
+	int idx = 0;
+	unsigned int sum = 0;
+
+	// buffered copy for padding
+	for(int i = 0; i < numOfNeighbors; i++)
+		buf[i] = *p1;
+
+	for( int i = numOfNeighbors; i < numOfNeighbors + length; i++) {
+		buf[i] = p1[idx];
+		idx += jump;
+	}
+
+	for(int i = numOfNeighbors + length; i < bufSize; i++) {
+		buf[i] = p1[idx - jump];
+	}
+
+	for(int i = 0; i < width; i++)
+		sum += buf[i];
+	for(int i = 0; i < length; i++) {
+		int pos = i*jump;
+		p2[pos] = sum / width;
+		sum += (buf[width + i] - buf[i]);
+	}
+	delete[] buf;
+}
+
+
 void
 HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 {
@@ -18,6 +54,7 @@ HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 	int total = w * h;
 
 	ChannelPtr<uchar> p1, p1_temp, p2, endPtr;
+	int type;
 
 	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++ ) {
 		IP_getChannel(I1_temp, ch, p1_temp, type);
@@ -28,10 +65,17 @@ HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 				*p1_temp++ = *p1++;
 		}
 
-		if( filterW > 1) {
+		else if( filterW > 1) {
 			// blur rows y = 0 to y = h
 			// should I create a function for this
+			for(int y = 0; y < h; y++) {
+				row_Blur(p1, p1_temp, w, filterW, 1);
+				p1 += w;
+				p1_temp += w;
+			}
 		}
+
+		p1_temp = p1_temp - total;
 
 		// blurr the temp image and output to I2
 		if( filterH == 1) {
@@ -39,9 +83,14 @@ HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 				*p2++ = *p1_temp++;
 		}
 
-		if(filterH > 1) {
-			// copy all the blurred rows
+		else if(filterH > 1) {
+			// blurr columns
 			// should I create a function for this
+			for(int x  = 0; x < w; x++) {
+				row_Blur(p1_temp, p2, h, filterH, w);
+				p1_temp += 1;
+				p2 += 1;
+			}
 		}
 	}
 }
