5ee6b071ab5a533b244b60861b25c2de48557761
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-29 00:40:46 -0400

    median filtering initial

--- Assignments/hw2/HW_median.cpp
+++ Assignments/hw2/HW_median.cpp
@@ -9,7 +9,36 @@ using std::vector;
 // Clamp sz to 9.
 // Output is in I2.
 //
+
+
+// need a function to get the median of  of the neighbor
+
+// need a function to copy each row seperately
+
 void
 HW_median(ImagePtr I1, int sz, ImagePtr I2)
 {
+    int w = I1->width();
+    int h = I1->height();
+    int total = w * h;
+
+    IP_copyImageHeader(I1, I2);
+    int numOfNeighbors = 0;
+
+    ChannelPtr<uchar> p1, p2, endPtr;
+    int type;
+
+	// make the row odd as usual
+	if(sz % 2 == 0)
+		sz++;
+
+	if(sz == 1) {
+		// just copy like I did on sharpening.. hah hah
+	}
+
+	else {
+		// do the processing.... concentrate
+		// for each row, copy and process
+		
+	}
 }
