68402345dc122cdfd26d03fd5f80e8095b09afa4
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-30 01:54:04 -0400

    median filtering with slight different output

--- Assignments/hw2/HW_median.cpp
+++ Assignments/hw2/HW_median.cpp
@@ -14,7 +14,8 @@ using std::vector;
 // need a function to get the median of  of the neighbor
 
 // need a function to copy each row seperately
-void bufferedCopy(ChannelPtr<uchar> P, short* buf, int kernelSize, int width);
+void bufferedRowCopy(ChannelPtr<uchar> P, short* buf, int kernelSize, int width);
+int medianOfNeighbors(std::vector<int> neighbors, int numOfNeighbors);
 
 void
 HW_median(ImagePtr I1, int sz, ImagePtr I2)
@@ -24,55 +25,100 @@ HW_median(ImagePtr I1, int sz, ImagePtr I2)
     int total = w * h;
 
     IP_copyImageHeader(I1, I2);
-    int numOfNeighbors = 0;
+    int numOfNearestNeighbors = 0;
 
     ChannelPtr<uchar> p1, p2, endPtr;
     int type;
 
-    if(sz > 9)
-    	sz  = 9;
 	// make the row odd as usual
 	if(sz % 2 == 0)
 		sz++;
 
 	if(sz == 1) {
 		// just copy like I did on sharpening.. hah hah
+		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++){ 
+			IP_getChannel(I2, ch, p2, type);
+			for(endPtr = p1 + total; p1 < endPtr; )
+				*p2++ = *p1++;
+		}
+	}
+	else if (sz > 1) {
+		// do the processing.... concentrate
+		short* buf[sz];
+		int length = sz + w - 1;
+		for(int i = 0; i < sz; i++)
+			buf[i] = new short[length];
+
+		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+			IP_getChannel(I2, ch, p2, type);
+			endPtr = p1 + total;
+			int px;
+			for(px = 0; px < sz/2; px++)
+				bufferedRowCopy(p1, buf[px], sz, w);
+			for( ; px < sz; px++ ) {
+				bufferedRowCopy(p1, buf[px], sz, w);
+				p1  = p1 + w;
+			}
+
+			std::vector<int> neighbors(0);
+			for(int y = 0; y < h; y++) {
+				for(int j = 0; j < sz; j++) {
+					for(int i = 0; i < sz; i++)
+						neighbors.push_back(buf[i][j]);
+				}
+
+				for(int x = 0; x < w; x++) {
+					*p2++ = medianOfNeighbors(neighbors, numOfNearestNeighbors);
+					if( x < w - 1) {
+						neighbors.erase(neighbors.begin(), neighbors.begin()+sz);
+						for(int i = 0; i < sz; i++)
+							neighbors.push_back(buf[i][sz+x]);
+					}
+				}
+				neighbors.clear();
+				bufferedRowCopy(p1, buf[(sz + y -1)%sz], sz, w);
+				if(p1 < endPtr - w)
+					p1 = p1 + w;
+
+			}
+		}
+		for(int i = 0; i < sz; i++)
+			delete[] buf[i];
 	}
 
-	else {
-		// do the processing.... concentrate
-		// for each row, copy and process
-		
-	}
 }
 
 // function to copy a row pixels into a buffered array
-
-void bufferedCopy(ChannelPtr<uchar> P, short* buf, int kernelSize, int width) {
-	int i = 0, bufSize = kernelSize + width - 1;
+void bufferedRowCopy(ChannelPtr<uchar> P, short* buf, int kernelSize, int width) {
+	int i = 0;
 	// copying the row pixels
 	for( ; i < kernelSize / 2; i++)
 		buf[i] = *P;
 	for( ; i < kernelSize/2+width -1; i++)
 		buf[i] = *P++;
-	for( ; i < bufSize; i ++)
+	for( ; i < kernelSize + width - 1; i ++)
 		buf[i] = *P;
 }
 
-int medianOfKNeighbors(std::vector<int> elements, int numOfNeighbors) {
-	int total = elements.size();
+int medianOfNeighbors(std::vector<int> neighbors, int numOfNeighbors) {
+	int total = neighbors.size();
 	if(total == 1)
-		return elements[0];
+		return neighbors[0];
 	else {
-		std::sort(elements.begin(), elements.end());
+		std::sort(neighbors.begin(), neighbors.end());
 		int mid = total / 2;
-		if(numOfNeighbors == 0)
-			return elements[mid];
+		if(numOfNeighbors == 0){
+			if(total %2 != 0)
+				return neighbors[mid];
+			else
+				return (neighbors[mid] + neighbors[mid-1])/2;
+		}
+
 		else {
-			short sum = elements[mid];
-			int i;
+			short sum = neighbors[mid];
+			int i = 0;
 			for(i = 1; i <= numOfNeighbors; i++)
-				sum = (sum + elements[mid-i] + elements[mid+i]);
+				sum = (sum + neighbors[mid-i] + neighbors[mid+i]);
 
 			return sum/(2*i + 1);
 		}
