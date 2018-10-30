e687d30588a5ada9a78fa1f858ade1239c2ae318
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-29 23:32:46 -0400

    defined median of n nearest neighbor

--- Assignments/hw2/HW_median.cpp
+++ Assignments/hw2/HW_median.cpp
@@ -14,6 +14,7 @@ using std::vector;
 // need a function to get the median of  of the neighbor
 
 // need a function to copy each row seperately
+void bufferedCopy(ChannelPtr<uchar> P, short* buf, int kernelSize, int width);
 
 void
 HW_median(ImagePtr I1, int sz, ImagePtr I2)
@@ -28,6 +29,8 @@ HW_median(ImagePtr I1, int sz, ImagePtr I2)
     ChannelPtr<uchar> p1, p2, endPtr;
     int type;
 
+    if(sz > 9)
+    	sz  = 9;
 	// make the row odd as usual
 	if(sz % 2 == 0)
 		sz++;
@@ -40,5 +43,39 @@ HW_median(ImagePtr I1, int sz, ImagePtr I2)
 		// do the processing.... concentrate
 		// for each row, copy and process
 		
+	}
+}
+
+// function to copy a row pixels into a buffered array
+
+void bufferedCopy(ChannelPtr<uchar> P, short* buf, int kernelSize, int width) {
+	int i = 0, bufSize = kernelSize + width - 1;
+	// copying the row pixels
+	for( ; i < kernelSize / 2; i++)
+		buf[i] = *P;
+	for( ; i < kernelSize/2+width -1; i++)
+		buf[i] = *P++;
+	for( ; i < bufSize; i ++)
+		buf[i] = *P;
+}
+
+int medianOfKNeighbors(std::vector<int> elements, int numOfNeighbors) {
+	int total = elements.size();
+	if(total == 1)
+		return elements[0];
+	else {
+		std::sort(elements.begin(), elements.end());
+		int mid = total / 2;
+		if(numOfNeighbors == 0)
+			return elements[mid];
+		else {
+			short sum = elements[mid];
+			int i;
+			for(i = 1; i <= numOfNeighbors; i++)
+				sum = (sum + elements[mid-i] + elements[mid+i]);
+
+			return sum/(2*i + 1);
+		}
+
 	}
 }
