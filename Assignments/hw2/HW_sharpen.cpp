4cc7afd3ee89a2e5589175eb57d351a1ea4f4b6c
Bashir Ahamed <ahamedb1991@gmail.com>
2018-10-31 00:36:41 -0400

    convolve slight different histogram

--- Assignments/hw2/HW_sharpen.cpp
+++ Assignments/hw2/HW_sharpen.cpp
@@ -4,16 +4,11 @@
 // Sharpen image I1. Output is in I2.
 //
 
-
-
-
 // how to call HW_blur ????
-
 // fixed it... using extern
-
 extern void row_Blur(ChannelPtr<uchar> p1, ChannelPtr<uchar> p2, int length, int width, int jump);
-void
-extern HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2);
+
+extern void HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2);
 
 void
 HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
@@ -27,7 +22,7 @@ HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
 	ChannelPtr<uchar> p1, p2, endPtr;
 	int type;
 
-	// no need to process if size is 1
+	// no need to process if filter size is 1
 	if(size == 1) {
 		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
 			IP_getChannel(I2, ch, p2, type);
@@ -40,7 +35,6 @@ HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
 		// blur the image
 		// ? Why not call the HW_blur function... nice
 		// multiply with factor with the blurred diff and add with original value
-
 		HW_blur(I1, size, size, I2);
 		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
 			IP_getChannel(I2, ch, p2, type);
